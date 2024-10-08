72d48f47d83d369c1630e5e7ee0272ea31991324
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2024-10-08 22:29:07 +0700

    update tree

---
+++ Data_Structures/data_structure_implementations/tree/c sharp/Tree/Program.cs
@@ -0,0 +1,21 @@
+﻿using DataStructures.Tree;
+
+Tree<int> tree = new Tree<int>(new Node<int>(1));
+Node<int> root = tree.Root;
+Node<int> child1 = new Node<int>(2);
+Node<int> child2 = new Node<int>(3);
+tree.AddNode(root, child1);
+tree.AddNode(root, child2);
+Node<int> child3 = new Node<int>(4);
+tree.AddNode(child1, child3);
+
+Console.WriteLine("Height: " + tree.Height(root));  // 3
+Console.WriteLine("Size: " + tree.Size(root));    // 4
+
+
+Console.Write("\nBreadth First Traverse:");
+tree.BreadthFirstTraverse(root, node => Console.Write(node.Data + " "));  // 1 2 3 4
+Console.WriteLine();  // Thêm dòng mới sau khi in Breadth First Traverse
+Console.Write("\nDepth First Traverse:");
+tree.DepthFirstTraverse(root, node => Console.Write(node.Data + " "));    // 1 2 4 3
+Console.WriteLine();  // Thêm dòng mới sau khi in Depth First Traverse
---
+++ Data_Structures/data_structure_implementations/tree/c sharp/Tree/Tree.cs
@@ -0,0 +1,117 @@
+/*
+    Implementing Tree in C#
+    By: Hoang Hiep
+*/
+
+namespace DataStructures.Tree
+{
+
+    public class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+        public Tree(Node<T> root)
+        {
+            Root = root;
+        }
+
+        public void AddNode(Node<T> parent, Node<T> node)
+        {
+            parent.AddChild(node);
+        }
+
+        public void RemoveNode(Node<T> parent, Node<T> node)
+        {
+            parent.RemoveChild(node);
+        }
+
+        public int Height(Node<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int maxHeight = 0;
+                foreach (var child in node.Children)
+                {
+                    int childHeight = Height(child);
+                    if (childHeight > maxHeight)
+                    {
+                        maxHeight = childHeight;
+                    }
+                }
+                return 1 + maxHeight;
+            }
+        }
+
+        public int Size(Node<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int size = 1;
+                foreach (var child in node.Children)
+                {
+                    size += Size(child);
+                }
+                return size;
+            }
+        }
+
+
+        public void DepthFirstTraverse(Node<T> node, Action<Node<T>>? action = null)
+        {
+            action = action ?? (n => Console.WriteLine(n.Data));
+            action(node);
+            foreach (var child in node.Children)
+            {
+                DepthFirstTraverse(child, action);
+            }
+        }
+
+        public void BreadthFirstTraverse(Node<T> node, Action<Node<T>>? action = null)
+        {
+            action = action ?? (n => Console.WriteLine(n.Data));
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                Node<T> current = queue.Dequeue();
+                action(current);
+                foreach (var child in current.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public List<Node<T>> Children { get; set; } = new List<Node<T>>();  // used List instead of array to make it easier to add and remove children
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public void AddChild(Node<T> node)
+        {
+            Children.Add(node);
+        }
+
+        public void RemoveChild(Node<T> node)
+        {
+            Children.Remove(node);
+        }
+
+        public override string ToString()
+        {
+            return Data?.ToString() ?? string.Empty;
+        }
+    }
+}
